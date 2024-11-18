4d71b90c1722950a53d60f97e46fbb138eacf873
shawon1fb <shawon0187@gmail.com>
2024-11-18 20:58:50 +0600

    Initial Commit

---
+++ Medium_UI/Medium_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_UIApp.swift
+//  Medium_UI
+//
+//  Created by shahanul on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Medium_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Medium_UITests/Medium_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  Medium_UITests.swift
+//  Medium_UITests
+//
+//  Created by shahanul on 11/18/24.
+//
+
+import Testing
+@testable import Medium_UI
+
+struct Medium_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
