24d3a51391227e9a13b58025e3149b72dfb98113
shawon1fb <shawon0187@gmail.com>
2025-01-06 22:57:26 +0600

    feat(Presentation): Simplify and standardize PostDetailsContainer's code structure and view hierarchy.

--- Medium_UI/Presentation/PostDetails/PostDetailsContainer.swift
+++ Medium_UI/Presentation/PostDetails/PostDetailsContainer.swift
@@ -5,29 +5,28 @@
 //  Created by shahanul on 12/16/24.
 //
 
-import SwiftUI
 import EasyX
+import SwiftUI
 import XSwiftUI
 
 struct PostDetailConatiner: View {
   let post: PostSingleItem
-    
-    @StateObject var postDetailsVM: PostDetailsViewModel
-    
-    init(post: PostSingleItem) {
-        self.post = post
-        _postDetailsVM = StateObject(wrappedValue: PostDetailsViewModelBindings().getDependencies(post: post))
-    }
-    
-    
+
+  @StateObject var postDetailsVM: PostDetailsViewModel
+
+  init(post: PostSingleItem) {
+    self.post = post
+    _postDetailsVM = StateObject(
+      wrappedValue: PostDetailsViewModelBindings().getDependencies(post: post))
+  }
+
   var body: some View {
     ScrollView {
       VStack(alignment: .leading, spacing: 16) {
-        Text(post.title)
+
         Text(post.title)
           .font(.largeTitle)
           .bold()
-         
 
         HStack {
           Text(post.creator.name)
@@ -37,10 +36,9 @@ struct PostDetailConatiner: View {
             .foregroundColor(.secondary)
         }
         Divider()
-          PostBodyContentView(post: post)
+        PostBodyContentView(post: post)
       }
-        
-     
+
       .padding()
       .textSelection(.enabled)
 
@@ -49,77 +47,65 @@ struct PostDetailConatiner: View {
   }
 }
 
-
-
 struct PostBodyContentView: View {
-    let post: PostSingleItem
-      
-    @ObservedObject var viewModel: PostDetailsViewModel
-      
-      init(post: PostSingleItem) {
-          self.post = post
-          _viewModel = ObservedObject(wrappedValue: PostDetailsViewModelBindings().getDependencies(post: post))
-      }
-      
-    
-    var body: some View {
-        VStack{
-            if let hasError = viewModel.hasError{
-                
-                VStack{
-                    Text(hasError)
-                        .font(.headline)
-                        .foregroundColor(.red)
-                }
-            }else{
-          
-                
-               
-                Text(viewModel.title)
-                    .font(.title)
-                    .padding()
-                Text(viewModel.subtitle)
-                    .font(.subheadline)
-                    .padding()
-                
-                if viewModel.loading{
-                    ProgressView()
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                    
-                    
-                }else{
-                    ScrollView {
-                        VStack(alignment: .leading, spacing: 0) {
-                            ForEach(viewModel.paragraphs) { paragraph in
-                                ParagraphView(paragraph: paragraph)
-                            }
-                        }
-                        .padding()
-                    }
-                }
-                
+  let post: PostSingleItem
+
+  @ObservedObject var viewModel: PostDetailsViewModel
+
+  init(post: PostSingleItem) {
+    self.post = post
+    _viewModel = ObservedObject(
+      wrappedValue: PostDetailsViewModelBindings().getDependencies(post: post))
+  }
+
+  var body: some View {
+    VStack {
+      if let hasError = viewModel.hasError {
+
+        VStack {
+          Text(hasError)
+            .font(.headline)
+            .foregroundColor(.red)
+        }
+      } else {
+
+        if viewModel.loading {
+          ProgressView()
+            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+
+        } else {
+          ScrollView {
+            VStack(alignment: .leading, spacing: 0) {
+              ForEach(viewModel.paragraphs) { paragraph in
+                ParagraphView(paragraph: paragraph)
+              }
             }
-          
+            .padding()
+          }
         }
-//        .onChange(of: post.id) { _ in
-//                 Task {
-//                     await viewModel.getPostContentByID(postID: post.id)
-//                 }
-//             }
-        .onAppear(perform: {
-            print("onAppear called for post \(post.id)")
-        })
-        .task {
-            print("task called for post \(post.id)")
-          //  await viewModel.getPostContentByID(postID: post.id)
-        }
+
+      }
+
     }
+    //        .onChange(of: post.id) { _ in
+    //                 Task {
+    //                     await viewModel.getPostContentByID(postID: post.id)
+    //                 }
+    //             }
+    .onAppear(perform: {
+      print("onAppear called for post \(post.id)")
+    })
+    .task {
+      print("task called for post \(post.id)")
+      //  await viewModel.getPostContentByID(postID: post.id)
+    }
+  }
 }
 
-struct PostDetailsBody:View {
-    var body: some View {
-        VStack{
-            
-        }
+struct PostDetailsBody: View {
+  var body: some View {
+    VStack {
+
     }
+  }
 }
